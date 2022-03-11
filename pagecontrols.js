047545dfc6526f2e5bdb1206c3092313c9bf3d2c
David Allingham <David.Allingham@newcastle.edu.au>
2022-03-11 15:23:17 +1100

    Added ability to zoom the graph in and out using the mouse wheel.

--- pagecontrols.js
+++ pagecontrols.js
@@ -34,8 +34,8 @@ function getTranslate(objId){
   // match two ints/floats inside parentheses, preceeded by "translate":
   var translateExpr = RegExp(/translate\(([-]?\d+[.]?\d*) ([-]?\d+[.]?\d*)\)/).exec(transform);
   if (translateExpr!=null){
-   oldtranslate[0] = parseInt(translateExpr[1]);
-   oldtranslate[1] = parseInt(translateExpr[2]);
+   oldtranslate[0] = parseFloat(translateExpr[1]);
+   oldtranslate[1] = parseFloat(translateExpr[2]);
   }
   return oldtranslate;
  } else {
@@ -52,8 +52,8 @@ function getScale(objId){
   // match two ints/floats inside parentheses, preceeded by "scale":
   var scaleExpr = RegExp(/scale\(([-]?\d+[.]?\d*) ([-]?\d+[.]?\d*)\)/).exec(transform);
   if (scaleExpr!=null){
-   oldscale[0] = parseInt(scaleExpr[1]);
-   oldscale[1] = parseInt(scaleExpr[2]);
+   oldscale[0] = parseFloat(scaleExpr[1]);
+   oldscale[1] = parseFloat(scaleExpr[2]);
   }
   return oldscale;
  } else {
@@ -70,7 +70,7 @@ function getRotate(objId){
   // match two ints/floats inside parentheses, preceeded by "rotate":
   var rotateExpr = RegExp(/rotate\(([-]?\d+[.]?\d*)/).exec(transform);
   if (rotateExpr!=null){
-   oldrotate[0] = parseInt(rotateExpr[1]);
+   oldrotate[0] = parseFloat(rotateExpr[1]);
   }
   return oldrotate;
  } else {
@@ -88,7 +88,6 @@ function setTransform(objId,translate=null,scale=null,rotate=null){
   if (rotate==null) rotate = getRotate(objId);
   // form the 'transform' string
   var transform = `scale(${scale[0]} ${scale[1]}) rotate(${rotate[0]}) translate(${translate[0]} ${translate[1]})`;
-  console.log(transform);
   el.setAttribute("transform",transform);
  }
 }
@@ -116,7 +115,6 @@ function setDeltaTransform(objId,dtranslate=null,dscale=null,drotate=null){
 
   // form the 'transform' string
   var transform = `scale(${scale[0]} ${scale[1]}) rotate(${rotate[0]}) translate(${translate[0]} ${translate[1]})`;
-//  console.log(transform);
   el.setAttribute("transform",transform);
  }
 }
@@ -124,32 +122,42 @@ function setDeltaTransform(objId,dtranslate=null,dscale=null,drotate=null){
 function setTranslate(objId,translate=null){
  setTransform(objId,translate);
 }
+function setScale(objId,scale=null){
+ var translate = null; // don't change translation
+ var rotate = null; // don't change rotation
+ setTransform(objId,translate,scale,rotate);
+}
 
 function startDragGraph(){
  dragging = true;
- var mouseX = Math.round(event.clientX);
- var mouseY = Math.round(event.clientY);
- dragX0 = mouseX;
- dragY0 = mouseY;
-// console.log("Start drag at "+mouseX.toString()+", "+mouseY.toString());
+ // where is the mouse when dragging starts?
+ dragX0 = Math.round(event.clientX);
+ dragY0 = Math.round(event.clientY);
  // where is the object when dragging starts?
  dragOrigin = getTranslate("graph0");
 }
 function dragGraph(){
  if (dragging){
-  var dragStrength = 2;
+  var dragStrength = 1.5; // perhaps this should scale with the zooming (scale) of the graph
   var offsetX = Math.round(event.clientX-dragX0);
   var offsetY = Math.round(event.clientY-dragY0);
-  console.log("Drag offset is "+offsetX.toString()+", "+offsetY.toString());
   setTransform("graph0",[dragOrigin[0]+dragStrength*offsetX,dragOrigin[1]+dragStrength*offsetY]); // could add scale, rotate
  }
 }
 function endDragGraph(){
  if (dragging){
   dragging = false;
-  var mouseX = Math.round(event.clientX);
-  var mouseY = Math.round(event.clientY);
-//  console.log("End drag at "+mouseX.toString()+", "+mouseY.toString());
+ }
+}
+function wheelZoom(){
+ // zoom the graph when the mouse wheel scrolls: note that positive deltaY is turned into "zoom OUT"
+ var zoomStrength = 0.25; // change the scroll by this in a step-wise manner
+ if (event.deltaY!=0){
+  var scale = getScale("graph0");
+  scale[0] += (event.deltaY>0? -zoomStrength : zoomStrength);
+  scale[0] = Math.min(10.0,Math.max(0.1,scale[0])); // minimum scale 0.1, maximum scale 10.0
+  scale[1] = scale[0];
+  setScale("graph0",scale);
  }
 }
 
@@ -158,3 +166,5 @@ dragging = false;
 document.getElementById("thegraph").addEventListener("mousedown",startDragGraph);
 document.getElementById("thegraph").addEventListener("mousemove",dragGraph);
 document.getElementById("thegraph").addEventListener("mouseup",endDragGraph);
+// use scroll wheel for zooming
+document.getElementById("thegraph").addEventListener("wheel",wheelZoom);
